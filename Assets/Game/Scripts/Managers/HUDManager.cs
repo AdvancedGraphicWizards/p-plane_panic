888cad7c22e9cdcea344b06d471e34bb8d55e8c1
DavidGiraldoCode <davidgiraldodesign@gmail.com>
2024-09-26 10:35:09 +0200

    HUD activates when game starts

---
+++ Assets/Game/Scripts/Managers/HUDManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDManager : MonoBehaviour
+{
+    public static HUDManager Instance { get; private set;}
+    [SerializeField] private GameState m_gameState;
+    [SerializeField] private GameObject m_fuelHUD;
+    [SerializeField] private GameObject m_distanceHUD;
+    private void Awake()
+    {
+        if (!Instance)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        
+
+    }
+
+}
