51173fdc08faa4c93ef03730e20a740c33dfbae9
udon <159450789+udon-udon@users.noreply.github.com>
2024-09-26 14:58:18 +0200

    Updated phone client UI

--- Assets/WebGL/Scripts/ClientUI.cs
+++ Assets/WebGL/Scripts/ClientUI.cs
@@ -9,20 +9,18 @@ public class ClientUI : Singleton<ClientUI>
     [Header("UI Elements")]
     [SerializeField] private TMP_InputField m_nameInputField;
     [SerializeField] private TMP_InputField m_gameCodeInputField;
-    [SerializeField] private TMP_Text m_joinButtonText;
+    //[SerializeField] private TMP_Text m_joinButtonText;
     [SerializeField] private GameObject m_joinScreen;
     [SerializeField] private GameObject m_playScreen;
     [Tooltip("UI Element that shows the player color, can be an image or a the background.")]
     [SerializeField] private Image m_playerColorObject;
+    [SerializeField] private TMP_Text m_playerNameText;
+    [SerializeField] private TMP_Text m_playerNameBackdrop;
 
-    // Private members
-    [Header("Phone Controller Reference")]
-    public PhoneController phoneController;
-
-    [Header("UI Elements")]
+    [Header("Connection Variables")]
     public string playerName = "Player";
     [SerializeField] private Color m_playerColor = new Color(0.5235849f, 1.0f, 0.6483989f);
-    [SerializeField] private bool isConnected = false;
+    [SerializeField] private bool m_isConnected = false;
 
     private void Awake()
     {
@@ -50,65 +48,54 @@ public class ClientUI : Singleton<ClientUI>
 
     private void Start()
     {
-        // Nothing for now
+        if (SystemInfo.supportsGyroscope) Input.gyro.enabled = true;
     }
 
     public void JoinGame()
     {
-        if (!(m_joinButtonText.text == "Connecting"))
+        Debug.Log("Joining game");
+
+        string name = m_nameInputField.text;
+        string code = m_gameCodeInputField.text;
+
+        if (name == "")
         {
-            Debug.Log("Joining game");
-
-            string name = m_nameInputField.text;
-            string code = m_gameCodeInputField.text;
-
-            if (name == "")
-            {
-                Debug.Log("Name cannot be empty!");
-                return;
-            }
-
-            if (code == "")
-            {
-                Debug.Log("Game code cannot be empty!");
-                return;
-            }
-
-            if (name.Length > 12)
-            {
-                Debug.Log("Name must be 12 characters or less!");
-                return;
-            }
-
-            Debug.Log($"Attempting to join game with code {code} as {name}");
-
-            //phoneController.SetName(name);
-            playerName = name;
-            Connect(code);
-
-            m_joinButtonText.text = "Connecting";
+            Debug.Log("Name cannot be empty!");
+            return;
         }
 
-        else
+        if (code == "")
         {
-            Disconnect();
-            m_joinButtonText.text = "Connect";
+            Debug.Log("Game code cannot be empty!");
+            return;
         }
+
+        if (name.Length > 12)
+        {
+            Debug.Log("Name must be 12 characters or less!");
+            return;
+        }
+
+        Debug.Log($"Attempting to join game with code {code} as {name}");
+
+        //phoneController.SetName(name);
+        m_playerNameText.text = m_playerNameBackdrop.text = playerName = name;
+        Connect(code);
     }
 
     public void IsConnected(bool connectionStatus)
     {
-        if (connectionStatus != isConnected)
+        if (connectionStatus != m_isConnected)
         {
-            isConnected = connectionStatus;
+            m_isConnected = connectionStatus;
             m_playScreen.SetActive(connectionStatus);
             m_joinScreen.SetActive(!connectionStatus);
 
             // Do something
 
-            if (isConnected)
+            if (m_isConnected)
             {
-                m_joinButtonText.text = "Connect";
+                // Do something
             }
 
             else
@@ -119,11 +106,11 @@ public class ClientUI : Singleton<ClientUI>
 
         // Else same status we had previously, do nothing.
     }
-    
-    public void ChangeColor(Color newColor) {
+
+    public void ChangeColor(Color newColor)
+    {
         m_playerColor = newColor;
-        m_playerColorObject.color = m_playerColor;
-        
+        m_playerColorObject.material.SetColor("_BaseColor", m_playerColor);
     }
 
     public void Connect(string joinCode)
