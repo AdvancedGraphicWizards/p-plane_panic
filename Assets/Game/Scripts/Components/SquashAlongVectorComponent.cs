e3b87bfaac498e14a1ee2ea2459f75087d80c3b1
Canaguia <filipgunnarkarlsson@gmail.com>
2024-10-24 11:52:59 +0200

    Added blinking

--- Assets/Game/Scripts/Components/SquashAlongVectorComponent.cs
+++ Assets/Game/Scripts/Components/SquashAlongVectorComponent.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem.Processors;
+using UnityEngine.InputSystem.Utilities;
 
 public class SquashAlongVectorComponent : MonoBehaviour
 {
     public Vector3 squashDirection = new Vector3(1, 1, 0); // Arbitrary squash vector
     public float squashFactor = 0.5f; // Factor to scale along the direction
+    float stretchFactor = 0f; // Factor to scale along the direction
 
     private GameObject m_scaleParent;
     private GameObject m_rotationGrandparent;
+
+    private Vector3 previousPos;
 
     void Start()
     {
@@ -18,21 +22,32 @@ public class SquashAlongVectorComponent : MonoBehaviour
 
         // set scaleparents forward direction to squashdirection
         m_scaleParent.transform.forward = squashDirection;
+        m_scaleParent.transform.localPosition = transform.localPosition;
 
         // set rotationgrandparents rotation and position to the objects original position and rotation
-        m_rotationGrandparent.transform.rotation = transform.localRotation;
-        m_rotationGrandparent.transform.localPosition = transform.localPosition;
+        m_rotationGrandparent.transform.rotation = transform.rotation;
+        m_rotationGrandparent.transform.localPosition = m_scaleParent.transform.position;
 
         // Set parent hiearchy
         transform.parent = m_scaleParent.transform;
         m_scaleParent.transform.parent = m_rotationGrandparent.transform;
+    }
 
-        StretchAlongVector(Vector3.one, 2, false);
+    private void Update()
+    {
+        Vector3 movementVector = transform.position - previousPos;
+        float stretchFactorTarget = Vector3.Magnitude(movementVector)*3f;
+        stretchFactor = Mathf.Max(Mathf.Lerp(stretchFactor, stretchFactorTarget, Time.deltaTime),1);
+        StretchAlongVector(movementVector, stretchFactor, true);
+        previousPos = transform.position;
     }
 
     public void StretchAlongVector(Vector3 direction, float factor, bool maintain_volume)
     {
+        if (Vector3.zero == direction) direction = Vector3.one;
         Vector3 scaleVector;
+        m_scaleParent.transform.forward = direction;
+        transform.rotation = m_rotationGrandparent.transform.rotation;
 
         if (maintain_volume) {
             float inverseFactor = 1/Mathf.Sqrt(1 + factor);
